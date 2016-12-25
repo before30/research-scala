79852c5c3610eced96d593f808e4b1bb2f76bc73
before30 <before30@gmail.com>
2016-12-25 23:49:34 +0900

    Add TestMain object

---
+++ src/main/scala/cc/before30/examples/TestMain.scala
@@ -0,0 +1,11 @@
+package cc.before30.examples
+
+/**
+  * Created by before30 on 25/12/2016.
+  */
+object TestMain {
+  def main(args: Array[String]): Unit = {
+    println("Hello world")
+  }
+
+}
